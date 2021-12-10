750509b1c4f0bbab2e72e215ec4fd1905123da4c
Daniel Meyer Dammous <dannydam@gmail.com>
2021-12-09 23:18:54 -0300

    init

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const serverless = require('serverless-http')
+const serverless = require('serverless-http');
 const fs = require('fs');
 const app = express();
 const port = 3110;
