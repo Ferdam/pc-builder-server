9c8e46c0f0b7f5397770526159661ed971fa0f48
Daniel Meyer Dammous <dannydam@gmail.com>
2021-12-10 18:48:00 -0300

    cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const fs = require('fs');
+var cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient } = require('mongodb');
@@ -10,6 +11,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 let collection = null;
 let database = null;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
